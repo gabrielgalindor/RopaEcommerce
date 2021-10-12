fc1a9bad612966988181c5e17286e4b004859d23
unknown <gabrielgalindorodriguez@gmail.com>
2021-10-12 17:10:53 -0500

    Moda-Psycho Galindo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,7 @@ export const NavBar = () =>{
                     <button class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
                         <span class="d-flex align-items-center">
                             <i class="bi-chat-text-fill me-2"></i>
-                            <span class="small">Log In</span>
+                            <span class="small">Compras</span>
                         </span>
                     </button>
                 </div>
