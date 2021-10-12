3ec277eb3e18f16ca69c4809f571badeac70e8c7
unknown <gabrielgalindorodriguez@gmail.com>
2021-10-12 17:08:07 -0500

    Moda-Psycho NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/NavBar.css'
+import {NavBar} from './components/NavBar.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar> </NavBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Menu.js
@@ -0,0 +1,5 @@
+export const Menu = () =>{
+    return (
+        <h1> Buena la banda </h1>
+    );
+}
